b557296d0fe167f0b4ea86f65b29e782b5fada2d
kimzliang <740872230@qq.com>
2017-09-17 22:18:08 +0800

    update

--- 9.17/npm/index.js
+++ 9.17/npm/index.js
@@ -1,5 +1,12 @@
 var plus = require('./util/plus.js')
 
+// var vue = require('vue')
+
+modal()
+// var webpack = require('webpack')
+
+// var fs = require('fs')
+
 console.log(plus(3, 4))
 
-console.log(process.env.npm_package_config_port)
+// console.log(process.env.npm_package_config_port)
